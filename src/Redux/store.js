b8d37920f80b42b4ea3994cfc5646f10f0304b9d
illuri raviteja reddy <107460626+raviwed@users.noreply.github.com>
2023-01-02 21:53:57 +0530

    Delete store.js

--- src/Redux/store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, legacy_createStore } from "redux";
-import thunk from "redux-thunk"
-import {reduser} from "./reduser"
-export const store=legacy_createStore(reduser,applyMiddleware(thunk))
-console.log(store)
